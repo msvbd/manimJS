2d0edd333d14e564eedf82ab4c2dc69cc562acbb
msvbd <svobod.martin@gmail.com>
2022-09-28 20:52:58 +0200

    rename bundle name

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,17 +5,17 @@ module.exports = {
   entry: "./ts/app.ts",
   watch: true,
   output: {
-    filename: "app.js",
+    filename: "manimjs.bundle.js",
     path: path.resolve(__dirname, "js")
   },
   devtool: "inline-source-map",
   module: {
     rules: [
-      { test: /(\.d)?\.(ts)x?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.(ts)x?$/, use: 'ts-loader', exclude: /node_modules/ },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".d.ts"],
+    extensions: [".ts", ".tsx", ".js"],
   },
 };
